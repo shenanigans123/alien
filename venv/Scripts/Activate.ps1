c66acc2baefee76d68ad46fc93500454d0ee76f9
Shani <shani.munir@rackspace.co.uk>
2020-01-08 14:58:48 +0000

    restructuring

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="D:\Programs\Python\Projects\alien\venv"
+$env:VIRTUAL_ENV="C:\Storage\Python\alien\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=D:\Programs\Python\Projects\alien\venv"
+set "VIRTUAL_ENV=C:\Storage\Python\alien\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
